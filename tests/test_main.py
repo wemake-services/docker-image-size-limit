5e5a6385a5a40ba0e966bfb16e25e8c3a1442af0
sobolevn <mail@sobolevn.me>
2019-03-03 13:11:43 +0300

    Fixes flake8

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,12 +2,11 @@
 
 import pytest
 
-from docker_image_size_limit import sys, main
+from docker_image_size_limit import main, sys
 
 
 def test_main_overflow(docker_client, image_name, monkeypatch):
     """Checks size with binary limit."""
-
     monkeypatch.setattr(sys, 'argv', ['', image_name, '1 MB'])
     with pytest.raises(SystemExit) as exit_value:
         main()
@@ -17,7 +16,6 @@ def test_main_overflow(docker_client, image_name, monkeypatch):
 
 def test_main_correct(docker_client, image_name, monkeypatch):
     """Checks size with binary limit."""
-
     monkeypatch.setattr(sys, 'argv', ['', image_name, '1 GB'])
     with pytest.raises(SystemExit) as exit_value:
         main()
