782bb665dfe3c72920d3471e47699e3043af7585
sobolevn <mail@sobolevn.me>
2021-02-14 15:56:12 +0300

    wps@0.15.1

--- docker_image_size_limit/__init__.py
+++ docker_image_size_limit/__init__.py
@@ -13,9 +13,7 @@ if sys.version_info >= (3, 8):  # pragma: no cover
 else:  # pragma: no cover
     import importlib_metadata  # noqa: WPS433
 
-    def lib_version(package: str) -> str:  # noqa: WPS440
-        """Stub function to get the project's version."""
-        return importlib_metadata.version(package)  # type: ignore
+    lib_version = importlib_metadata.version
 
 #: We use this variable to show version spec.
 _version = lib_version(
