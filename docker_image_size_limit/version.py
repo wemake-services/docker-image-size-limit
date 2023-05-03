9de0147c3c9ffbbafa3b65a2de9801e7f39bf80c
sobolevn <mail@sobolevn.me>
2023-05-03 12:25:57 +0300

    Version 1.0.0 release

--- docker_image_size_limit/version.py
+++ docker_image_size_limit/version.py
@@ -1,11 +1,6 @@
-import sys
-
-if sys.version_info >= (3, 8):  # pragma: no cover
-    from importlib import metadata as importlib_metadata  # noqa: WPS433
-else:  # pragma: no cover
-    import importlib_metadata  # noqa: WPS440, WPS433
+from importlib import metadata
 
 
 def get_version(distribution_name: str) -> str:
     """Our helper to get version of a package."""
-    return importlib_metadata.version(distribution_name)  # type: ignore
+    return metadata.version(distribution_name)  # type: ignore
