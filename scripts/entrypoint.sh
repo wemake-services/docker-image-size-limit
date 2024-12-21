1bfa7df42cbd2d7d0eeecff7e9bdf5809a8dedad
Fabio C. Barrionuevo da Luz <bnafta@gmail.com>
2024-12-21 14:13:50 -0300

    Add --current-size, --exit-zero
    and allow execute it as module via
    python -m docker_image_size_limit

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
 
-# Default values:
+# Passed args from GitHub Actions:
+: "${INPUT_IMAGE:=$1}"  # Required
+: "${INPUT_SIZE:=$2}"  # Required
+: "${INPUT_MAX_LAYERS:=$3}"  # Optional
+: "${INPUT_SHOW_CURRENT_SIZE:=$4}"  # Optional
+: "${INPUT_EXIT_ZERO:=$5}"  # Optional
+
+# Default values, needed because `Dockerfile` can be used directly:
+# These values must match ones in `action.yml`!
 : "${INPUT_MAX_LAYERS:=-1}"
+: "${INPUT_SHOW_CURRENT_SIZE:=false}"
+: "${INPUT_EXIT_ZERO:=false}"
 
 # Diagnostic output:
 echo "Using image: $INPUT_IMAGE"
 echo "Size limit: $INPUT_SIZE"
 echo "Max layers: $INPUT_MAX_LAYERS"
+echo "Show Current Size: $INPUT_SHOW_CURRENT_SIZE"
+echo "Exit Zero: $INPUT_EXIT_ZERO"
 echo 'disl --version:'
 disl --version
 echo '================================='
 echo
 
+SHOW_CURRENT_SIZE_FLAG=''
+if [ "$INPUT_SHOW_CURRENT_SIZE" = 'true' ]; then
+  SHOW_CURRENT_SIZE_FLAG='--current-size'
+fi
+
+EXIT_ZERO_FLAG=''
+if [ "$INPUT_EXIT_ZERO" = 'true' ]; then
+  EXIT_ZERO_FLAG='--exit-zero'
+fi
+
+
 # Runs disl:
-output=$(disl "$INPUT_IMAGE" "$INPUT_SIZE" --max-layers="$INPUT_MAX_LAYERS")
+output=$(disl "$INPUT_IMAGE" "$INPUT_SIZE" --max-layers="$INPUT_MAX_LAYERS" "$SHOW_CURRENT_SIZE_FLAG" "$EXIT_ZERO_FLAG")
 status="$?"
 
 # Sets the output variable for Github Action API:
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,6 @@
 import subprocess
+import sys
+from os.path import basename
 
 
 def test_disl_exceeds_limit(image_name: str) -> None:
@@ -58,3 +60,92 @@ def test_disl_max_layers(image_name: str) -> None:
     assert process.returncode == 1
     assert 'maximum layers by' in output
     assert image_name in output
+
+
+def test_disl_current_size(image_name: str) -> None:
+    """Runs `disl` command with --current-size flag."""
+    process = subprocess.Popen(
+        [
+            'disl',
+            image_name,
+            '1 kb',
+            '--current-size',
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        encoding='utf8',
+    )
+    output, _ = process.communicate()
+
+    assert process.returncode == 1
+    assert 'current size is' in output
+    assert image_name in output
+
+
+def test_disl_exit_zero(image_name: str) -> None:
+    """Runs `disl` command with --exit-zero flag."""
+    process = subprocess.Popen(
+        [
+            'disl',
+            image_name,
+            '1 kb',
+            '--current-size',
+            '--exit-zero',
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        encoding='utf8',
+    )
+    output, _ = process.communicate()
+
+    assert process.returncode == 0
+    assert 'current size is' in output
+    assert image_name in output
+
+
+def test_docker_image_size_limit_as_module(image_name: str) -> None:
+    """Runs `disl` command with --exit-zero flag."""
+    interpreter_binary_name = basename(sys.executable)
+    process = subprocess.Popen(
+        [
+            '{0}'.format(interpreter_binary_name),
+            '-m',
+            'docker_image_size_limit',
+            image_name,
+            '1 kb',
+            '--current-size',
+            '--exit-zero',
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        encoding='utf8',
+    )
+    output, _ = process.communicate()
+
+    assert process.returncode == 0
+    assert 'current size is' in output
+    assert image_name in output
+
+
+def test_docker_image_size_limit_as_module_help_flag(image_name: str) -> None:  # noqa: WPS118,E501
+    """Runs `disl` command via it python module."""
+    interpreter_binary_name = basename(sys.executable)
+    process = subprocess.Popen(
+        [
+            '{0}'.format(interpreter_binary_name),
+            '-m',
+            'docker_image_size_limit',
+            '--help',
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        encoding='utf8',
+    )
+    output, _ = process.communicate()
+
+    assert process.returncode == 0
+    assert 'docker_image_size_limit' in output
